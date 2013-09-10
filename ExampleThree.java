4e059f6840dde243986309a94d3b805cfbf67a91
CJ Gehin-Scott <cjgehinscott@me.com>
2013-09-10 11:23:17 -0400

    End of Class
    Examples of if and else if statements as well as an updated circle
    program with else if statements added.

---
+++ ExampleThree.java
@@ -0,0 +1,40 @@
+//CJ Gehin-Scott
+
+import java.util.Scanner;
+
+public class ExampleThree{
+  public static void main(String[] args){
+    
+    /*boolean bool = true;
+    System.out.println(bool);
+    bool = false;
+    System.out.println(bool);
+    boolean x = (1 == 0);
+    System.out.println("X is " + x);*/
+    
+    Scanner inputBox = new Scanner(System.in);
+    int number;
+    System.out.println("Please enter a number: ");
+    number = inputBox.nextInt();
+    
+    /*if(number > 0){
+      System.out.println("The number " + number + " is positive.");
+    }
+    if(number < 0){
+    System.out.println("The number " + number + " is negative.");
+    }
+    if(number == 0){
+       System.out.println("The number " + number + " is neither negative or positive.");
+    }*/
+    
+    if(number > 0) {
+      System.out.println("The number " + number + " is positive.");
+    }else if(number < 0) {
+      System.out.println("The number " + number + " is negative.");
+    }else(number == 0) {
+      System.out.println("The number " + number + " is neither negative or positive.");
+    }
+    
+    
+  }//end of main method
+}//end of ExampleThree class
